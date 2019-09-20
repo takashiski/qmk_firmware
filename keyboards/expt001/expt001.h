1c74ac34961003c40f5c81d9a0f264f37fbf7c2e
takashiski <takashiskibb@gmail.com>
2019-09-20 20:15:51 +0900

    define LAYOUT

--- keyboards/expt001/expt001.h
+++ keyboards/expt001/expt001.h
@@ -25,5 +25,18 @@
  * The second converts the arguments into a two-dimensional array which
  * represents the switch matrix.
  */
-#define LAYOUT(k00, k01, k02, k10, k11) \
-    { {k00, k01, k02}, {k10, KC_NO, k11}, }
+#define LAYOUT(\
+    k00, k01, k02, k03,\
+    k10, k11, k12, k13,\
+    k20, k21, k22, k23,\
+    k30, k31, k32, k33,\
+    k41, k42, k43,\
+    k40\
+)  \
+{\                                                                                                                 \
+    {k00, k01, k02, k03},\
+    {k10, k11, k12, k13}, \
+    {k20, K21, K22, K23}, \
+    {k30, k31, k32, k33} \
+    {k40, k41, k42, k43 } \
+}
