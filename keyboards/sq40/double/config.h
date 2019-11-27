33493076cf3ec2a8d75adbd14320d4aa0d4c2548
takashiski <takashiskibb@gmail.com>
2019-11-28 00:05:13 +0900

    split single and double

--- keyboards/sq40/double/config.h
+++ keyboards/sq40/double/config.h
@@ -28,8 +28,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DESCRIPTION A custom keyboard
 
 /* key matrix size */
-#define MATRIX_ROWS 2
-#define MATRIX_COLS 3
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 8
 
 /*
  * Keyboard Matrix Assignments
