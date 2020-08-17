eea206b4e1a629b6038d0081cc9e88b7f28ba937
takashiski <takashiskibb@gmail.com>
2020-08-18 00:58:48 +0900

    update config.h

--- keyboards/jp60/config.h
+++ keyboards/jp60/config.h
@@ -27,8 +27,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT      jp60
 
 /* key matrix size */
-#define MATRIX_ROWS 2
-#define MATRIX_COLS 3
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 14
 
 /*
  * Keyboard Matrix Assignments
@@ -40,8 +40,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
  */
-#define MATRIX_ROW_PINS { D0, D5 }
-#define MATRIX_COL_PINS { F1, F0, B0 }
+#define MATRIX_ROW_PINS { B1,B2,B3,F0,F1 }
+#define MATRIX_COL_PINS { F4,F7,F5,F6,C7,C6,B6,B5,B4,D7,D6,D4,D5,D4 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL */
