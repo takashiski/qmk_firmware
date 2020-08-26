0ff57e2d777d53af14b7e9c1a6d577b31d3ecf63
takashiski <takashiskibb@gmail.com>
2020-08-26 21:21:39 +0900

    create jp60

--- keyboards/jp60/config.h
+++ keyboards/jp60/config.h
@@ -40,8 +40,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
  */
-#define MATRIX_ROW_PINS { B1,B2,B3,F0,F1 }
-#define MATRIX_COL_PINS { F4,F7,F5,F6,C7,C6,B6,B5,B4,D7,D6,D4,D5,D4 }
+#define MATRIX_ROW_PINS { B3,B2,B1,F0,F1 }
+#define MATRIX_COL_PINS { F4,F5,F6,F7,C7,C6,B6,B5,B4,D7,D6,D4,D5,D3 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL */
