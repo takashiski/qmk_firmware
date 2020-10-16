f2616c75256939b504bc4e3a677bbf2c4caf453e
takashiski <takashiskibb@gmail.com>
2020-10-17 01:27:32 +0900

    define basic

--- keyboards/otaku_split/v2p0/config.h
+++ keyboards/otaku_split/v2p0/config.h
@@ -40,8 +40,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
  */
-#define MATRIX_ROW_PINS { D0, D5 }
-#define MATRIX_COL_PINS { F1, F0, B0 }
+#define MATRIX_ROW_PINS { C6, D7, E6, B4, B5}
+#define MATRIX_ROW_PINS_RIGHT {B5, B4, E6, D7, C6}
+#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3, B2, D3 }
+#define MATRIX_COL_PINS_RIGHT { B6, B2, B3, B1, F7, F6, F5, F4}
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL */
@@ -144,3 +146,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Bootmagic Lite key configuration */
 //#define BOOTMAGIC_LITE_ROW 0
 //#define BOOTMAGIC_LITE_COLUMN 0
+#define B6_AUDIO
+#ifdef AUDIO_ENABLE
+  #define STARTUP_SONG SONG(STARTUP_SOUND)
+#endif
