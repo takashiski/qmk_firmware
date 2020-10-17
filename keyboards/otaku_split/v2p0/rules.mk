73ff88bb100a89b073a0aa941a8d6b1193d0f76a
takashiski <takashiskibb@gmail.com>
2020-10-17 13:23:34 +0900

    otaku v2p0

--- keyboards/otaku_split/v2p0/rules.mk
+++ keyboards/otaku_split/v2p0/rules.mk
@@ -2,12 +2,13 @@
 MCU = atmega32u4
 
 # Bootloader selection
-BOOTLOADER = atmel-dfu
+
 
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
+#BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
+BOOTMAGIC_ENABLE = full
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
@@ -20,4 +21,5 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = yes           # Audio output
+#AUDIO_ENABLE = no           # Audio output
 SPLIT_KEYBOARD = yes
