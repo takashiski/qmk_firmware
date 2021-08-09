177eff3b07721b00f2619f9a294d1b5c957b56e4
takashiski <takashiskibb@gmail.com>
2021-08-09 17:23:55 +0900

    レイヤー数を変更

--- keyboards/jp60/v1p1/keymaps/via/keymap.c
+++ keyboards/jp60/v1p1/keymaps/via/keymap.c
@@ -20,7 +20,9 @@
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
     _BASE,
-    _FN
+    _FN,
+    _GAME,
+    _CALC,
 };
 
 
@@ -37,7 +39,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_CAPS,  KC_LEFT,    KC_DOWN,    KC_RIGHT,   KC_END,    KC_INSERT,    KC_LEFT,    KC_DOWN,    KC_UP,    KC_RIGHT,    KC_SCLN, KC_QUOT, KC_BSLS, KC_PSCR,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    RESET,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP,   KC_RSFT,
   KC_LCTL, KC_LGUI, KC_LALT, KC_MHEN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HENK,KC_TRNS, KC_LEFT, KC_DOWN,  KC_RIGHT
-)
+),
+ [_GAME] = LAYOUT(
+  KC_GRAVE,  KC_1,  KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,   KC_JYEN,  KC_BSPC,
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_CAPS,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLS,KC_ENT,
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RO,   KC_RSFT,
+  KC_LCTL, KC_LGUI, KC_LALT, KC_MHEN, LT(_FN,KC_DEL), KC_SPC,  LT(_FN,KC_ENT), LT(_FN,KC_BSPC), KC_HENK, LT(_FN,KC_KANA),KC_RALT, KC_APP,  KC_RCTL
+ ),
+  [_CONFIG] = LAYOUT(
+  KC_GRAVE,  KC_1,  KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,   KC_JYEN,  KC_BSPC,
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_4,    KC_5,    KC_6,    KC_P,    KC_LBRC, KC_RBRC,
+  KC_CAPS,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_MINUS,    KC_1,    KC_2,    KC_3,    SHIFT(KC_SCLN), SHIFT(KC_QUOT), KC_BSLS,KC_ENT,
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_0,    KC_0, KC_DOT,  KC_SLSH, KC_RO,   KC_RSFT,
+  KC_LCTL, KC_LGUI, KC_LALT, KC_MHEN, LT(_FN,KC_DEL), KC_SPC,  LT(_FN,KC_ENT), LT(_FN,KC_BSPC), KC_HENK, LT(_FN,KC_KANA),KC_RALT, KC_APP,  KC_RCTL
+ )
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
