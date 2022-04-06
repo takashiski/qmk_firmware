1a46799129b271de56a995bc2d7d11d221f6237b
takashiski <takashiskibb@gmail.com>
2022-04-07 01:24:08 +0900

    create ps/2 trackball definition

---
+++ keyboards/takashiski/trackball/config.h
@@ -0,0 +1,37 @@
+// Copyright 2022 takashiski (@takashiski)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#ifdef PS2_USE_INT
+#define PS2_CLOCK_PIN D2
+#define PS2_DATA_PIN D3
+
+#define PS2_INT_INIT()  do {    \
+    EICRA |= ((1<<ISC21) |      \
+              (0<<ISC20));      \
+} while (0)
+#define PS2_INT_ON()  do {      \
+    EIMSK |= (1<<INT2);         \
+} while (0)
+#define PS2_INT_OFF() do {      \
+    EIMSK &= ~(1<<INT2);        \
+} while (0)
+#define PS2_INT_VECT   INT2_vect
+#endif
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
---
+++ keyboards/takashiski/trackball/rules.mk
@@ -0,0 +1,4 @@
+# This file intentionally left blank
+
+PS2_MOUSE_ENABLE = yes
+PS2_USE_INT = yes
